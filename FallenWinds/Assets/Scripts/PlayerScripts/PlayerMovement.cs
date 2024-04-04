046999a0492ba7f2a145f0554697bb27c1e95b44
wertiop1 <102436461+wertiop1@users.noreply.github.com>
2024-04-03 21:57:29 -0700

    Insert anim files, and add tiles

--- FallenWinds/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ FallenWinds/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,38 +5,79 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private PlayerInput p_input;
-    private Rigidbody2D rb;
-    private float movementX;
-    private float movementY;
 
-    public float speed = 1.00f;
+    private InputAction move;
 
+    public Rigidbody2D rb;
+    public Animator animator;
+    //public SpriteRenderer spriteRenderer;
+    public PlayerInputActions PlayerControls; 
+    
+    public float limit = 1f;
+    public float moveSpeed = 5.00f;
+
+    Vector2 moveDirection = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        p_input = new PlayerInput();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        //spriteRenderer = GetComponent<spriteRenderer>();
+    }
+    private void Awake() {
+        PlayerControls = new PlayerInputActions();
     }
 
-    private void OnEnable()
-    {
-        p_input.Movement.Enable();
+    private void OnEnable() {
+        move = PlayerControls.Player.Move;
+        move.Enable();
     }
-    
-    private void OnDisable()
-    {
-        p_input.Disable();
+
+    private void OnDisable() {
+        move.Disable();
     }
 
     private void FixedUpdate() {
-
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
-    // Update is called once per frame
-    void Update()
+/*
+
+    private bool tryMove(Vector2 direction, float limit)
     {
-        if(p_input.Movement.Up.WasPressedThisFrame) {
-            Debug.Log("Up");
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            direction,
+            limit
+            );
+        if (!hit)
+        {
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+            return true;
+        } else {
+            Debug.Log("hit:" + hit.collider.name);
+            return false;
         }
     }
+*/
+    void Update()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+        if(moveDirection.x > 0)
+        {
+            animator.SetBool("isMoving", true);
+            animator.SetBool("isSide", true);
+            //spriteRenderer.flipX = false;
+            //transform.Rotation.y = 0;
+        } else if (moveDirection.x < 0) 
+        {
+            animator.SetBool("isMoving", true);
+            animator.SetBool("isSide", true);
+            //spriteRenderer.flipX = true;
+            //transform.Rotation.y = -180;
+        }
+        
+    }
+
 }
+
